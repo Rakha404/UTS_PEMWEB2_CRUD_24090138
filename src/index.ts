3aa70687233fee06f3313c755fb9b39ce4ce4a93
tito <titosukamenabung@gmail.com>
2026-06-10 18:36:18 +0700

    push gatau

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import eventRoutes from './routes/eventRoute.js';
+import categoryRoutes from './routes/categoryRoute.js';
+import pembicaraRoutes from './routes/pembicaraRoute.js';  
+import authRoute from './routes/authRoute.js'
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("ini adalah api untuk invofest");
+});
+
+app.use("/events", eventRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/pembicara", pembicaraRoutes);
+app.use("/auth", authRoute);  
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
---
+++ src/routes/eventRoute.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { createEvent, deleteEvent, getEvent, showEvent, updateEvents } from "../controllers/eventController.js";
+
+const router = express.Router();
+
+router.get("/", getEvent);
+router.post("/", createEvent); //menyimpan data event
+router.get("/id", showEvent); //menampilkan data event berdasarkan id
+router.put("/:id", updateEvents); //mengupdate data event berdasarkan id
+router.delete("/:id", deleteEvent); //menghapus data event berdasarkan id
+
+export default router;
